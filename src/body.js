aad47aafa11a1ee09e83f2dee4128664865cb3aa
Bart Busschots <opensource@bartificer.net>
2022-09-03 16:22:59 +0100

    Refactored code to use Webpack for local code only

---
+++ src/body.js
@@ -0,0 +1,4 @@
+// TO DO — 3rd-party includes
+
+// Import local JS Code
+import './index.js';
---
+++ src/head.js
@@ -0,0 +1,12 @@
+//
+// import the CSS files that should be loaded in the HTML head
+//
+
+// import bootstrap CSS
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import font awesome CSS
+//import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// custom CSS
+import './index.css';
